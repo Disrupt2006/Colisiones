47b1fd43d962cc19094d00e802b4d4f0fa5d060b
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-05-26 10:36:42 -0300

    Ejemplo colisiones

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float RotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //f para que tome como float
+            //transform.position += new Vector3(0, 0, 0.1f);
+            //usa sistema de coordenadas de objeto
+            transform.Translate(0, 0, MovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0,-MovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {   
+            //translate para rotar
+            transform.Rotate(0, RotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -RotationSpeed, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "DeathWall")
+        {
+            //gameObject sin mayus hace referencia al mismo objeto
+            Destroy(gameObject);
+        }
+    }
+}
